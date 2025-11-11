aa8578174a568008f5fff646fa00483fd7559909
JoacoG11 <101676686+JoacoG11@users.noreply.github.com>
2025-11-11 16:05:30 +0100

    Add suma.py to sum two numbers

---
+++ suma.py
@@ -0,0 +1,12 @@
+# suma.py
+
+# Función que suma dos números
+def sumar(a, b):
+    return a + b
+
+# Dos variables con números a elección
+num1 = 8
+num2 = 12
+
+# Mostrar la suma de los dos números usando la función
+print("La suma de", num1, "y", num2, "es:", sumar(num1, num2))
